87501f591379c1b7cc95a11ea57e105572afcf6b
Daniel Bright <daniel.bright@salesforce.com>
2025-05-19 13:31:23 -0700

    fixed logging tests

--- test/logger.ts
+++ test/logger.ts
@@ -7,25 +7,21 @@
 
 import { LoggerLevel } from "@salesforce/core";
 import { expect } from "chai";
-import baseLogger from "../src/utils/base-logger";
+import testBaseLogger from "../src/utils/base-logger";
 
 describe("Logger", () => {
   describe("Basic logger functionality", () => {
     it("should have a basic message", async () => {
-      const testbasiclogger = (
-        await baseLogger.child("testLogger")
-      ).useMemoryLogging();
+      const testbasiclogger = testBaseLogger.child("basic");
       testbasiclogger.warn("This is a test message");
       const logRecords = testbasiclogger.getBufferedRecords();
-      expect(logRecords[0]).to.have.property("msg", "This is a test message");
+      expect(logRecords.filter(lR => lR.name === "appLogger:basic")[0]).to.have.property("msg", "This is a test message");
     });
     it("should check for proper escaping", async () => {
-      const testescapelogger = (
-        await baseLogger.child("testLogger")
-      ).useMemoryLogging();
+      const testescapelogger = testBaseLogger.child("escape");
       testescapelogger.info('Checking escaping: "test" \\o/ foo=bar');
       const logRecords = testescapelogger.getBufferedRecords();
-      expect(logRecords[0]).to.have.property(
+      expect(logRecords.filter(lR => lR.name === "appLogger:escape")[0]).to.have.property(
         "msg",
         'Checking escaping: "test" \\o/ foo=bar'
       );
@@ -33,40 +29,30 @@ describe("Logger", () => {
   });
   describe("Logger levels", () => {
     it("should set the log level to a number: 50 for Error", async () => {
-      const errorlogger = (
-        await baseLogger.child("testLogger")
-      ).useMemoryLogging();
+      const errorlogger = testBaseLogger.child("error50");
       errorlogger.error("test Error");
       const logRecords = errorlogger.getBufferedRecords();
-      expect(logRecords[0]).to.have.property("level", 50);
+      expect(logRecords.filter(lR => lR.name === "appLogger:error50")[0]).to.have.property("level", 50);
     });
     it("should set the log level to a number: 40 for Warn", async () => {
-      const warnlogger = (
-        await baseLogger.child("testLogger")
-      ).useMemoryLogging();
+      const warnlogger = testBaseLogger.child("warn40");
       warnlogger.warn("test warn");
       const logRecords = warnlogger.getBufferedRecords();
-      expect(logRecords[0]).to.have.property("level", 40);
+      expect(logRecords.filter(lR => lR.name === "appLogger:warn40")[0]).to.have.property("level", 40);
     });
     it("should set the log level to a number: 30 for Info", async () => {
-      const infologger = (
-        await baseLogger.child("testLogger")
-      ).useMemoryLogging();
+      const infologger = testBaseLogger.child("info30");
       infologger.info("test info");
       const logRecords = infologger.getBufferedRecords();
-      expect(logRecords[0]).to.have.property("level", 30);
+      expect(logRecords.filter(lR => lR.name === "appLogger:info30")[0]).to.have.property("level", 30);
     });
     it("should set the log level to a number: 20 for Debug", async () => {
-      const debuglogger = (
-        await baseLogger.child("testLogger")
-      ).useMemoryLogging();
+      const debuglogger = testBaseLogger.child("debug20");
       debuglogger.setLevel(20);
       expect(debuglogger.getLevel()).to.equal(LoggerLevel.DEBUG);
     });
     it("should set the log level to a number: 10 for Trace", async () => {
-      const tracelogger = (
-        await baseLogger.child("testLogger")
-      ).useMemoryLogging();
+      const tracelogger = testBaseLogger.child("trace10");
       tracelogger.setLevel(10);
       expect(tracelogger.getLevel()).to.equal(LoggerLevel.TRACE);
     });
