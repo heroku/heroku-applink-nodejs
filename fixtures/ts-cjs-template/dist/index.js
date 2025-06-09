34a4eaba9ae3a0c59b3c638270ee92e5dae8f4d5
Elliot Kozil <152196227+ekozilforce@users.noreply.github.com>
2025-06-09 13:52:01 -0500

    update copyright message year (#44)
    * update request to extend headers passed (#40)
    
    * update request to extend headers passed
    
    * yarn format:write
    
    * update changelog
    
    * fix inconsistent capitalization

--- fixtures/ts-cjs-template/dist/index.js
+++ fixtures/ts-cjs-template/dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 /*
- * Copyright (c) 2024, salesforce.com, inc.
+ * Copyright (c) 2025, Salesforce.com, Inc.
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
